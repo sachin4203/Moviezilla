9e3a04e5d2624e9b933eb9ae25f5a6794d3f3f26
Sachin Bakshi <sbakshi36@gmail.com>
2016-03-12 17:53:42 +0530

    Removed the API key

--- app/src/main/java/com/example/sachin/moviezilla/MovieInterface.java
+++ app/src/main/java/com/example/sachin/moviezilla/MovieInterface.java
@@ -9,26 +9,11 @@ import retrofit.http.GET;
  */
 public interface MovieInterface {
 
-    //So these are the list available in our WEB API and the methods look straight forward
 
-   /* @GET("/Array.txt")
-    public void getArray(Callback<List<String>> items);
 
-    @GET("/ArrayWithObjects.txt")
-    public void getArrayWithObjects(Callback<List<Movie>> callback);
-
-    @GET("/Object.txt")
-    public void getObject(Callback<Movie> student);
-
-    @GET("/ObjectWithNestedArray.txt")
-    public void getObjectWithNestedArray(Callback<Movie> student);
-
-    @GET("/ObjectWithNestedObject.txt")
-    public void getObjectWithNestedObject(Callback<Movie> student);*/
-
-    @GET("/movie?vote_average.gte=7&vote_average.lte=10&sort_by=vote_count.desc&api_key=b67ba58a9ec094a58f2a82340e134ba1")
+    @GET("/movie?vote_average.gte=7&vote_average.lte=10&sort_by=vote_count.desc&api_key=[Insert Your API Key Here]")
     public void fetchPopularMovies(Callback<Movie> movie);
 
- @GET("/movie?primary_release_year=2015&sort_by=popularity.desc&api_key=b67ba58a9ec094a58f2a82340e134ba1")
+ @GET("/movie?primary_release_year=2015&sort_by=popularity.desc&api_key=[Insert Your API Key Here]")
  public void fetchMovies(Callback<Movie> movie);
 }
--- app/src/main/java/com/example/sachin/moviezilla/RestClient.java
+++ app/src/main/java/com/example/sachin/moviezilla/RestClient.java
@@ -4,9 +4,7 @@ package com.example.sachin.moviezilla;
  * Created by Sachin on 8/10/2016.
  */
 public class RestClient {
-    //You need to change the IP if you testing environment is not local machine
-    //or you may have different URL than we have here
-   // private static final String URL = "http://instinctcoder.com/wp-content/uploads/2015/08/";
+
     private static final String URL = "http://api.themoviedb.org/3/discover/";
 
     private retrofit.RestAdapter restAdapter;
